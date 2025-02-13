e317ffdc0630abde84d399cf3f90baf2945ab2ed
prathmeshbiradar <psbiradar948@gmail.com>
2025-02-13 13:40:51 +0530

    Implemented code to add image from database

--- src/main/java/com/ecommerce/backend/ecom_project/model/Product.java
+++ src/main/java/com/ecommerce/backend/ecom_project/model/Product.java
@@ -1,10 +1,7 @@
 package com.ecommerce.backend.ecom_project.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,11 +25,16 @@ public class Product {
     private String desc;
     private String brand;
     private double price;
-    private String Category;
-    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
+    private String category;
+//    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
     private Date releaseDate;
     private boolean available;
     private int quantity;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
 
 
 }
